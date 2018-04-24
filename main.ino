d114c65945bd6342d2fb1767e81f85f777d9b104
NicGobbi <38389716+NicGobbi@users.noreply.github.com>
2018-04-24 20:08:40 -0300

    Add files via upload
    Esse aqui é só um exemplo de como podemos usar a classe Position 
    Matheus você já pode começar a transcrever o seu fluxograma em forma de código aqui.

---
+++ main.ino
@@ -0,0 +1,14 @@
+#include <Position.h>
+
+void setup() {
+  Position atual(4, 3);
+
+}
+
+void loop() {
+  Position.Acquire();
+  float latToGo, longToGo;
+  if(abs(latToGo - atual.getLatitude) > eps && abs(longToGo - atual.getLongitude) > eps){
+    // Então aqui a gente coloca a rotina pro carrinho começar a andar em direção a longToGo e latToGo
+  }
+}
