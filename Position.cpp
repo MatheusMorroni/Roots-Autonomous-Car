ed16913520eff9fa838010da51858bb4d8d07bfa
NicGobbi <38389716+NicGobbi@users.noreply.github.com>
2018-04-24 19:27:15 -0300

    Create Position.cpp

---
+++ Position.cpp
@@ -0,0 +1,33 @@
+#include "Position.h"
+
+
+Position::Position(int rX, int tX)
+{
+    TinyGPS *GPS = new TinyGPS;
+    SoftwareSerial *rxTx = new SoftwareSerial(rX, tX);
+}
+
+Position::~Position()
+{
+    //dtor
+}
+
+void Position::Acquire(){
+    unsigned long start = millis();
+  do
+  {
+    while (ss.available())
+      GPS.encode(rxTx().read());
+  } while (millis() - start < 1000);
+    GPS.f_get_position(&this->latitude, &this->longitude, &age);
+}
+
+float Position::getLongitude()
+{
+    return longitude;
+}
+
+float Position::getLatitude()
+{
+    return latitude;
+}
