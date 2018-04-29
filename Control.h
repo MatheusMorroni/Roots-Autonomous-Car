dcac8e5cf0b69d157cf611498f8e33f469e3be24
gabriel2anjos <gabriel.anjos.dt@gmail.com>
2018-04-29 17:40:25 -0300

    Sketch headers

--- Control.h
+++ Control.h
@@ -1,1 +1,22 @@
+#ifndef CONTROL_H
+#define CONTROL_H
 #include "Ports.h"
+#include <Servo.h>
+#define MAX_VEL_VOLT 1024
+
+class Control {
+    Servo servo;
+    public:
+        Control();
+        //Aceita valores de -100 a 100(ré e frente) para vel e 0 a 10 para accel
+        void speedAccel(int _speed, unsigned short _accel);
+        //Mesmo que acima mas sem acelerar (abrupto)
+        void speed(int _speed);
+        //Recebe valor de -90 a 90 graus (0=centro)
+        void turn(int _turn);
+        //Retorna rotacao do servo
+        int servoState();
+};
+
+
+#endif
---
+++ Ports.h
@@ -0,0 +1,21 @@
+/* Aqui serao defininidas as portas a serem utilizadas. null é indefinida. Este arquivo deve ser incluido nos arquivos que mexam com hardware, no .cpp e no .h*/
+#ifndef PORTS_H
+#define PORTS_H
+
+//potencia
+#define CONTROLE_MOTOR_F null
+#define CONTROLE_MOTOR_B null
+#define SERVO null
+
+//localizacao
+#define GPS_TX null
+#define GPS_RX null
+
+
+//Comm
+#define BLUE_RX null
+#define BLUE_TX null
+
+
+
+#endif
